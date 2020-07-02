7941e03578e8e3415711eb2ed11ec31fd1895615
RICARDO ACERO <acerocol67@gmail.com>
2020-07-02 17:08:42 +0200

    hero

--- src/componentes/Localizaciones.js
+++ src/componentes/Localizaciones.js
@@ -9,7 +9,7 @@ function Localizaciones(){
             <center><h2>UBICANOS</h2>
             <div className='btnGrupo'>
             <Link className='btnLocacion tooltipped' data-position="top" data-tooltip="Excelente Te estaremos esperando" to='ccav/bna'>Barcelona</Link>
-            <Link className='btnLocacion disabled tooltipped'data-position="top" data-tooltip="Excelente Te estaremos esperando" to='ccav/lec'>Lecheria</Link>
+            {/* <Link className='btnLocacion disabled tooltipped'data-position="top" data-tooltip="Excelente Te estaremos esperando" to='ccav/lec'>Lecheria</Link> */}
             <Link className='btnLocacion  tooltipped' data-position="top" data-tooltip="Excelente Te estaremos esperando" to='ccav/pto'>Puerto La Cruz</Link>
             </div>
             <h3>Donde te quede más cerca</h3>
